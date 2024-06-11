193002de68c59a4ab57545521f7ced5e512d82f0
AlejandroF <125092602+JesusAFuentes@users.noreply.github.com>
2024-06-10 17:21:46 -0700

    path to login, logout and logoutall

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,14 @@
 from rest_framework import routers
 from apps.user import views
+from django.urls import path
+from knox.views import LogoutView, LogoutAllView
+from apps.user.views import LoginCustomView
 
 
 router = routers.SimpleRouter()
 router.register('users', views.UserViewSet, 'users')
 urlpatterns = [
+    path('auth/login/', LoginCustomView.as_view(), name='knox-custom-login'),
+    path('auth/logout/', LogoutView.as_view(), name='knox-logout'),
+    path('auth/logoutall/', LogoutAllView.as_view(), name='knox-logoutall'),
 ] + router.urls
